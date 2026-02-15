eba54c3bdd3d066ae58ac3b0a9f42f93269c5519
lburakaya <lburakkaya@hotmail.com>
2026-02-15 15:05:25 +0300

    Update base.py

--- app/modules/channel/base.py
+++ app/modules/channel/base.py
@@ -5,19 +5,31 @@ class ChannelBase(ABC):
         self.owner_id=owner_id
         self.name=name
         self.category=category
-        self.status=status
+        self.status='active'
+        self.balance=0.0
+        
 
     @abstractmethod
-    def chechk_monetization_eligibility(self):
-        ...
+    def calculate_advertising_revenue(self):
+        """
+        Docstring for calculate_advertising_revenue
+        
+        :param self: Description
+        """
+        pass
+
 
     
 
 class PersonalChhannel(ChannelBase):
-    ...
+    def __init__(self, channel_id, owner_id, name, category, status,izlenme_sayisi):
+        super().__init__(channel_id, owner_id, name, category, status)
+
 
 class BrandChannel(ChannelBase):
-    ...
+    def __init__(self, channel_id, owner_id, name, category, status):
+        super().__init__(channel_id, owner_id, name, category, status)
 
 class KidsChannel(ChannelBase):
-    ...
+    def __init__(self, channel_id, owner_id, name, category, status):
+        super().__init__(channel_id, owner_id, name, category, status)
